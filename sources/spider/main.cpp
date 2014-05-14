a935bedcb0dfc11bdaaac795a024ed32d7b5ae39
lylenical <lylenical@gmail.com>
2014-05-14 20:13:13 +0800

    测试libevent成功

--- sources/spider/main.cpp
+++ sources/spider/main.cpp
@@ -1,44 +1,118 @@
-#include <iostream>
+#include "stdafx.h"
+#include "tools/fileio/ftabfile.h"
 
-#include "event2/event-config.h"
+const char MESSAGE[] = "Hello, World!\n";
+void listener_cb(struct evconnlistener *, evutil_socket_t,
+    struct sockaddr *, int socklen, void *);
+void conn_writecb(struct bufferevent *, void *);
+void conn_eventcb(struct bufferevent *, short, void *);
+void signal_cb(evutil_socket_t, short, void *);
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#ifdef EVENT__HAVE_SYS_TIME_H
-#include <sys/time.h>
-#endif
-#ifdef EVENT__HAVE_SYS_SOCKET_H
-#include <sys/socket.h>
-#endif
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#ifndef _WIN32
-#include <unistd.h>
-#endif
-#include <errno.h>
-
-#include <event.h>
-
-using namespace std;
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-#ifdef _WIN32
-	WORD wVersionRequested;
-	WSADATA wsaData;
-
-	wVersionRequested = MAKEWORD(2, 2);
-
-	(void) WSAStartup(wVersionRequested, &wsaData);
-
-    printf("ruck");
+#ifdef WIN32
+	WSADATA wsa_data;
+	WSAStartup(0x0201, &wsa_data);
 #endif
+    struct sockaddr_in sin;
+    CFTabFile tabFile;
+    if(tabFile.Load("config/server.ini"))
+    {
+        string server_ip = tabFile.GetString("addr","ip","");
+        int server_port = tabFile.GetInteger("addr","port",0);
+        printf(server_ip.c_str());
+        memset(&sin, 0, sizeof(sin));
+        sin.sin_family = AF_INET;
+        //sin.sin_addr.S_addr = inet_addr(server_ip.c_str());
+        sin.sin_port = htons(server_port);
+    }
+	struct event_base *base;
+	struct evconnlistener *listener;
+	struct event *signal_event;
 
-	/* Initalize the event library */
-	//event_init();
-    printf("ruck");
-	return (0);
+
+
+	base = event_base_new();
+	if (!base) {
+		fprintf(stderr, "Could not initialize libevent!\n");
+		return 1;
+	}
+
+	listener = evconnlistener_new_bind(base, listener_cb, (void *)base,
+	    LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
+	    (struct sockaddr*)&sin,
+	    sizeof(sin));
+
+	if (!listener) {
+		fprintf(stderr, "Could not create a listener!\n");
+		return 1;
+	}
+
+	signal_event = evsignal_new(base, SIGINT, signal_cb, (void *)base);
+
+	if (!signal_event || event_add(signal_event, NULL)<0) {
+		fprintf(stderr, "Could not create/add a signal event!\n");
+		return 1;
+	}
+
+	event_base_dispatch(base);
+
+	evconnlistener_free(listener);
+	event_free(signal_event);
+	event_base_free(base);
+
+	printf("done\n");
+	return 0;
+}
+
+void listener_cb(struct evconnlistener *listener, evutil_socket_t fd,
+    struct sockaddr *sa, int socklen, void *user_data)
+{
+	struct event_base *base = (struct event_base *)user_data;
+	struct bufferevent *bev;
+
+	bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+	if (!bev) {
+		fprintf(stderr, "Error constructing bufferevent!");
+		event_base_loopbreak(base);
+		return;
+	}
+	bufferevent_setcb(bev, NULL, conn_writecb, conn_eventcb, NULL);
+	bufferevent_enable(bev, EV_WRITE);
+	bufferevent_disable(bev, EV_READ);
+
+	bufferevent_write(bev, MESSAGE, strlen(MESSAGE));
+}
+
+void conn_writecb(struct bufferevent *bev, void *user_data)
+{
+	struct evbuffer *output = bufferevent_get_output(bev);
+	if (evbuffer_get_length(output) == 0) {
+		printf("flushed answer\n");
+		bufferevent_free(bev);
+	}
+}
+
+void conn_eventcb(struct bufferevent *bev, short events, void *user_data)
+{
+	if (events & BEV_EVENT_EOF) {
+		printf("Connection closed.\n");
+	} else if (events & BEV_EVENT_ERROR) {
+		printf("Got an error on the connection: %s\n",
+		    strerror(errno));/*XXX win32*/
+	}
+	/* None of the other events can happen here, since we haven't enabled
+	 * timeouts */
+	bufferevent_free(bev);
+}
+
+void signal_cb(evutil_socket_t sig, short events, void *user_data)
+{
+	struct event_base *base = (struct event_base *)user_data;
+	struct timeval delay = { 2, 0 };
+
+	printf("Caught an interrupt signal; exiting cleanly in two seconds.\n");
+
+	event_base_loopexit(base, &delay);
 }
 
---
+++ sources/spider/tools/fileio/ftabfile.h
@@ -0,0 +1,52 @@
+/*
+ * date:2011-7-22
+ * auther:liangrongze
+ * desc:工具库，表格文件
+*/
+
+#ifndef __FTABFILE_H__
+#define __FTABFILE_H__
+
+#include <map>
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef std::vector<std::string> ROWLINE;
+
+class CFTabFile{
+public:
+	int			m_Width;
+	int			m_Height;
+	std::vector<std::string> colList;
+	std::vector<std::string> rowList;
+	std::map<int,ROWLINE> allMap;
+private:
+	//bool		GetValue(int nRow, int nColumn, const char* lpRString, DWORD dwSize);
+	int			Str2Col(const char* szColumn);
+public:
+	CFTabFile();
+	~CFTabFile();
+	bool		Load(const char* FileName);
+	bool		Save(const char* FileName);
+	bool		LoadPack(const char* FileName);
+	int			FindRow(const char* szRow);//返回以1为起点的值
+	int			FindColumn(const char* szColumn);//返回以1为起点的值
+	string		Col2Str(int nCol);
+	int			GetWidth() { return m_Width;};
+	int			GetHeight() { return m_Height;};
+	string		GetString(int nRow, const char* szColumn, const char* lpDefault);
+	string		GetString(int nRow, int nColumn, const char* lpDefault);
+	string		GetString(const char* szRow, const char* szColumn, const char* lpDefault);
+	int 		GetInteger(int nRow, const char* szColumn, int nDefault);
+	int	    	GetInteger(int nRow, int nColumn, int nDefault);
+	int		    GetInteger(const char* szRow, const char* szColumn, int nDefault);
+	float		GetFloat(int nRow, const char* szColumn, float fDefault);
+	float		GetFloat(int nRow, int nColumn, float fDefault);
+	float		GetFloat(const char* szRow, const char* szColumn, float fDefault);
+	void		Clear();
+};
+
+#endif
